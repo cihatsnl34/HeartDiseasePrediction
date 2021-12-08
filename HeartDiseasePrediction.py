1daf8ec8ce9f1a0b210a22fc0408d68d75712dca
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2021-12-08 21:58:58 +0300

    knn and logistics regressior created

--- HeartDiseasePrediction.py
+++ HeartDiseasePrediction.py
@@ -8,8 +8,8 @@ Created on Sat Oct 16 23:30:46 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
-
-
+from sklearn.metrics import confusion_matrix
+import statsmodels.api as sm
 #2.veriOnisleme
 #2.1 veri yükleme
 veriler= pd.read_csv("heart.csv")
@@ -58,4 +58,38 @@ sc=StandardScaler()
 X_train= sc.fit_transform(x_train)
 X_test= sc.fit_transform(x_test)
 
+#OLS Raporuna göre eleme yapıyoruz P.values 0.05 den büyükse eliyoruz 
+X_l=x.iloc[:,[0,1,2,3,4,7,8,9,10]].values
+X_l=np.array(X_l, dtype=float)
+model= sm.OLS(y,X_l).fit()
+print(model.summary())
+#Çıkanlar : FastingBS, RestingECG
+x=x.iloc[:,[0,1,2,3,4,7,8,9,10]]
+X=x.values 
 
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.33,random_state=0)
+
+sc=StandardScaler()
+X_train= sc.fit_transform(x_train)
+X_test= sc.fit_transform(x_test)
+#Prediction-Classification
+
+#Logistic Classification
+
+from sklearn.linear_model import LogisticRegression
+logr=LogisticRegression(random_state=0)
+logr.fit(X_train,y_train)
+y_pred=logr.predict(X_test)
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+#0.84 başarı
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=11,metric='minkowski')
+#n_neighbors :komşu sayısını veririz yazmazsak eğer default olarak 5 alır 
+#metric: komşu ile kendi arasında ki mesafeyi ölçecek metriği belirtiriz ona göre ölçer.
+#sklearn.neigbors dökümantasyonunda detayları var
+knn.fit(X_train,y_train)
+y_pred=knn.predict(X_test)
+cm=confusion_matrix(y_test, y_pred)    
+print(cm)     
+#0,848
